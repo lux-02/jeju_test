188e72310860dd62f028e5043ac7ef3d1b82de94
lux00 <oys128950@gmail.com>
2025-08-04 21:15:43 +0900

    add to adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-1884359786783237" />
+      </Head>
       <body>
         <Main />
         <NextScript />
